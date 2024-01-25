acdf6a846bd94377cdcb32b7393519308974daa7
HamzaGbada <hamza.gbada@gmail.com>
2024-01-25 12:00:31 +0100

    [INIT] doc element

---
+++ src/base/doc_ement.py
@@ -0,0 +1,8 @@
+class DocElement:
+    def __init__(self, x, y, w, h, content_type, content):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.content_type = content_type
+        self.content = content
