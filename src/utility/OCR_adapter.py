052c5402f7062105f1cb12c408c39005782eb822
HamzaGbada <hamza.gbada@gmail.com>
2024-02-05 18:54:05 +0100

    [INIT] OCR ADAPTER paddle

--- src/utility/OCR_adapter.py
+++ src/utility/OCR_adapter.py
@@ -1,17 +1,29 @@
+import logging
+
 from src.utility.base_utils import BaseUtils
 
 
 class OCRAdapter:
+    """
+            output = {
+            bbox: List[List]
+            content: List[Any]
+        }
+    """
     @staticmethod
     def from_paddle_ocr(paddle_ocr_output):
-        for output in paddle_ocr_output:
-            for text_box in output:
-                print("j")
-                print(text_box[0])
-                # TODO: Convert text_box to the expected format of the mapper
-                K = BaseUtils.X1X2X3X4_to_xywh(sum(text_box[0], []))
-                print("K")
-                print(K)
+        bbox_content_pairs = [
+            (BaseUtils.X1X2X3X4_to_xywh(sum(text_box[0], [])), text_box[1][0])
+            for output in paddle_ocr_output
+            for text_box in output
+        ]
+
+        bbox, content = zip(*bbox_content_pairs)
+
+        return {
+            "bbox": list(bbox),
+            "content": list(content)
+        }
 
     @staticmethod
     def from_easy_ocr(easy_ocr_output):
