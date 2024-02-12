217c80ecc099db2904805cc4149a0c91b2971d49
HamzaGbada <hamza.gbada@gmail.com>
2024-02-12 17:55:55 +0100

    [init] setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@ from setuptools import setup, find_packages
 # Read the requirements from requirements.txt
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-
+version = '0.1-SNAPSHOT'
 setup(
-    name='your_package_name',
-    version='0.1',
-    packages=find_packages(),
+    name='DocumentAI-std',
+    version=version,
+    packages=find_packages('src', exclude=['src.test']),
+    package_dir={'': 'src'},  # Specify the root package directory
     install_requires=requirements,
-    entry_points={
-        'console_scripts': [
-            'your_script_name = your_package_name.module_name:main_function',
-        ],
-    },
+
     # Add other metadata like author, description, etc.
-    author='Your Name',
-    description='Description of your package',
+    author='Hamza Gbada',
+    description='The main standards for Latis Document AI project',
 )
+
+
