9ad1fb1089872b3b133b17875c5c6a69321c0058
HamzaGbada <hamza.gbada@gmail.com>
2024-03-05 18:21:24 +0100

    [ADD] VerticalAlignment

--- DocumentAI_std/base/doc_enum.py
+++ DocumentAI_std/base/doc_enum.py
@@ -74,3 +74,32 @@ class HorizontalAlignment(Enum):
     RIGHT = 1
     CENTER = 2
     LEFT = 3
+
+
+class VerticalAlignment(Enum):
+    """
+    Enum representing the relative position of a bounding box in a document.
+
+    Attributes:
+        TOP_HEIGHT (int): Bounding box exists at the top of the document.
+        CENTRAL_HEIGHT (int): Bounding box exists in the central part of the document.
+        BOTTOM_HEIGHT (int): Bounding box exists at the bottom of the document.
+
+    Description:
+    The position of a bounding box (BBT) in the document is categorized into three horizontal parts:
+    - TOP_HEIGHT: At the top of the document.
+    - CENTRAL_HEIGHT: In the middle of the document.
+    - BOTTOM_HEIGHT: At the bottom of the document.
+
+    The table below describes the variable 'Position':
+
+    | Position         | Description                                  |
+    |------------------|----------------------------------------------|
+    | TOP_HEIGHT       | BBT exists at the top of the document.       |
+    | CENTRAL_HEIGHT   | BBT exists in the central part of the document. |
+    | BOTTOM_HEIGHT    | BBT exists at the bottom of the document.    |
+    """
+
+    TOP = 1
+    CENTER = 2
+    BOTTOM = 3
