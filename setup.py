e751ef9e29044560cfdd8bd88683cbbbbb990fa9
HamzaGbada <hamza.gbada@gmail.com>
2024-04-27 16:03:07 +0100

    [UPDATE] readme

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
+from pathlib import Path
+
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()
-
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 setup(
     name="DocumentAI_std",
     version="0.2.6.dev1",
     # packages=find_packages(where='DocumentAI_std'),  # Include all packages within this directory
     packages=find_packages(exclude=["DocumentAI_std.tests"]),
     # package_dir={'': 'DocumentAI_std'},  # Set root package directory
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=required,
     url="",
     license="",
