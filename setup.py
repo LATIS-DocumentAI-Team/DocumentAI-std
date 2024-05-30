5be51bdf87d1abfdb59c2d54eec5fda4dd4f25cd
HamzaGbada <hamza.gbada@gmail.com>
2024-05-30 16:57:30 +0100

    [UPDATE] new version 0.2.8

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 from pathlib import Path
-
 from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()
+
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="DocumentAI_std",
-    version="0.2.7.dev1",
-    # packages=find_packages(where='DocumentAI_std'),  # Include all packages within this directory
+    version="0.2.8.dev1",
     packages=find_packages(exclude=["DocumentAI_std.tests"]),
-    # package_dir={'': 'DocumentAI_std'},  # Set root package directory
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=required,
-    url="",
-    license="",
+    url="https://github.com/LATIS-DocumentAI-Group/DocumentAI-std",
+    license="MIT",
     author="Hamza Gbada",
-    author_email="",
+    author_email="hamza.gbada@gmail.com",
+    python_requires=">=3.11, <3.13",
     description="The main standards for Latis Document AI project",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
 )
