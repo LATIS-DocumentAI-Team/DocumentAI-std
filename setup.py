001ba04f3f3616637c161aca32f891552b54c9f9
HamzaGbada <hamza.gbada@gmail.com>
2024-11-22 11:41:33 +0100

    [ADD] get equivalent word

--- setup.py
+++ setup.py
@@ -1,8 +1,26 @@
+import subprocess
+import sys
 from pathlib import Path
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()
+
+
+class SpacyModelInstallCommand(install):
+    """Custom installation class to run spaCy model download after installation"""
+
+    def run(self):
+        install.run(self)
+
+        try:
+            subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_sm"])
+            subprocess.check_call([sys.executable, "-m", "spacy", "download", "en_core_web_lg"])
+        except subprocess.CalledProcessError as e:
+            print(f"Error downloading spaCy model: {e}")
+            sys.exit(1)
+
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
@@ -26,4 +44,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
     ],
+    cmdclass={
+            'install': SpacyModelInstallCommand,
+        },
 )
